47dc07eec200484cb5b381719040c546ab968997
Mark Howe <mvahowe@gmail.com>
2025-03-06 17:30:30 +0100

    Working dynamic language

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import {Grid2} from "@mui/material";
 import {i18nContext, doI18n} from "pithekos-lib";
 
 function App() {
-    const i18n = useContext(i18nContext);
+    const {i18nRef} = useContext(i18nContext);
     return <Grid2 container spacing={2}>
         <Grid2 size={12}>
-            <h1>{doI18n("pages:core-new-project:stub_content", i18n)}</h1>
+            <h1>{doI18n("pages:core-new-project:stub_content", i18nRef.current)}</h1>
         </Grid2>
     </Grid2>
 }
